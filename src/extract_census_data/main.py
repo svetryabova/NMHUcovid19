973044ed11697af3351a8e52309f90a6d553c3db
Victor Glebov <glebovv3105@gmail.com>
2020-07-07 23:44:17 -0600

    Added number of features of interest

--- src/extract_census_data/main.py
+++ src/extract_census_data/main.py
@@ -19,3 +19,13 @@ for i in range(2, (4 * number_of_counties - 1), 4):
     state = tmp_lst[i + 1].rstrip('"').lstrip(' ')
     abbrev = state_abbreviations[state]
     print(tmp_lst[i].lstrip('"').rstrip('County') + abbrev)
+
+# features we want to use
+feature_numbers = [1, 27, 7, 30, 31, 35, 45, 47, 60]
+# convert from percent to fraction:
+# 7 - persons 65 and over
+# 30 - computer
+# 31 - Internet
+# 35 - insurance
+# 47 - poverty
+features_to_convert = [7, 30, 31, 35, 47]
