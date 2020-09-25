60cd1896fc27ce0805c5d13e71c8edfa3414050d
Victor Glebov <glebovv3105@gmail.com>
2020-09-24 21:25:20 -0600

    Added explanation of seir_predict() function

--- src/SEIR_modeling/seir_model.py
+++ src/SEIR_modeling/seir_model.py
@@ -1,9 +1,25 @@
 from datetime import timedelta, date
 import matplotlib.pyplot as plt
 
-def seir_predict(start_date=date.today(), prediction_period=10, N=331e6, s0=331e6, e0=100, i0=0, r0=0, d0=0,
+def seir_predict(start_date=date.today(), prediction_period=10, N=331e6, e0=100, i0=0, r0=0, d0=0,
                  virus_params={'alpha': 0.00113, 'beta': 0.219, 'epsilon': 0.5, 'gamma': 0.0714}):
-    S = [s0]
+    '''
+    This function makes predictions based on classic SEIR model
+    :param start_date: date of start of prediction
+    :param prediction_period: number of days to predict
+    :param N: total population
+    :param e0: number of initially exposed people
+    :param i0: number of initially infected
+    :param r0: number of initially recovered
+    :param d0: total number of deaths up to the day of prediction start
+    :param virus_params: dictionary with the parameters characterizing epidemic, where
+    alpha is the virus-induced fatality rate
+    beta is the probability of disease transmission
+    epsilon is inversely proportional to the incubation period
+    gamma is the recovery rate
+    :return:
+    '''
+    S = [N]
     E = [e0]
     I = [i0]
     R = [r0]
